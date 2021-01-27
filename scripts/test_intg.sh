836c9647779705c6e295a5278189558a6f9ff44c
Sean Kelleher <ezanmoto@gmail.com>
2021-01-27 21:16:38 +0000

    Allow running specific integration test

--- scripts/test_intg.sh
+++ scripts/test_intg.sh
@@ -2,20 +2,31 @@
 # Use of this source code is governed by an MIT
 # licence that can be found in the LICENCE file.
 
-# `$0` runs the integration tests for this project.
+# `$0 [test-name]` runs the integration tests for this project, or just the
+# integration test called `test-name` if the optional argument is provided.
 
 set -o errexit
 
+if [ $# -gt 1 ] ; then
+    echo "usage: $0 [test-name]" >&2
+    exit 1
+fi
+test_name="$1"
+
 for conf in tests/*.yaml ; do
-    test_name=$(basename "$conf" | sed 's/\..*//')
-    echo -n "$test_name..."
+    cur_test_name=$(basename "$conf" | sed 's/\..*//')
+    if [ ! -z "$test_name" ] && [ "$cur_test_name" != "$test_name" ] ; then
+        continue
+    fi
+
+    echo -n "$cur_test_name..."
     python3 comment_style.py \
-        "tests/$test_name.yaml" \
-        &> "target/tests/$test_name.act.txt" \
+        "tests/$cur_test_name.yaml" \
+        &> "target/tests/$cur_test_name.act.txt" \
         || true
     diff \
         --unified \
-        "tests/$test_name.exp.txt" \
-        "target/tests/$test_name.act.txt"
+        "tests/$cur_test_name.exp.txt" \
+        "target/tests/$cur_test_name.act.txt"
     echo " ok"
 done
